32878b4685b9c48f4ff944b59014e8a8e0f54af2
dtee1 <davidtalson0@gmail.com>
2023-03-09 16:52:24 -0500

    9-setup-client Created client script

---
+++ client.py
@@ -0,0 +1,92 @@
+import socketio
+from flask import Flask, render_template, Response, jsonify
+import torch
+from torchvision import transforms
+import numpy
+import cv2
+import io
+from PIL import Image
+
+sio = socketio.Client()
+
+app = Flask(__name__)
+
+
+@sio.on("response")
+def response(frame):
+    frames.append(frame["compressed_image"])
+    image_size.append(frame["size_before"])
+    fps_host.append(frame["FPS"])
+    image_size_a.append(frame["size_after"])
+    frames_b.append(frame["from"])
+
+
+def generate_frame(type_):
+    if type_ == "frame_after":
+        while True:
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + frames[len(frames) - 1] + b"\r\n"
+            )
+    else:
+        while True:
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n"
+                + frames_b[len(frames_b) - 1]
+                + b"\r\n"
+            )
+
+
+@app.route("/video_after")
+def video_after():
+    return Response(
+        generate_frame("frame_after"),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
+
+
+@app.route("/video_before")
+def video_before():
+    return Response(
+        generate_frame("frame"),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
+
+
+@app.route("/size_b")
+def size_b():
+    return jsonify(image_size[len(image_size) - 1])
+
+
+@app.route("/fps")
+def fps():
+    return jsonify(fps_host[len(fps_host) - 1])
+
+
+@app.route("/size_a")
+def size_a():
+    return jsonify(image_size_a[len(image_size_a) - 1])
+
+
+@app.route("/com_fac")
+def com_fac():
+    result = image_size[len(image_size) - 1] / image_size_a[len(image_size_a) - 1]
+    return jsonify(int(result))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    frames = [b"None"]
+    image_size = []
+    fps_host = []
+    image_size_a = []
+    frames_b = [b"None"]
+
+    sio.connect("http://10.0.0.100:5000")
+    sio.emit("message", {"from": "client"})
+    app.run(port="8080")
